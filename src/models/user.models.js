7fd5f3fc73a7777aeea5d28fc6e3e1270f85b765
Igor Batista Lima <igorbatistalima1@gmail.com>
2025-10-02 09:25:47 -0300

    Refatora o método de comparação de senha para usar bcrypt de forma assíncrona e garante que a senha seja hasheada antes de salvar o usuário.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -29,8 +29,14 @@ const userSchema = new mongoose.Schema({
     timestamps: true,
   });
 
-userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compareSync(password, this.password);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
