be46d72435bd1f1edcd6565dc3427d413cfc5c80
Igor Batista Lima <igorbatistalima1@gmail.com>
2025-10-17 19:05:57 -0300

    refatora modelo de usuário para incluir importação de crypto e ajustes de formatação

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,39 +1,42 @@
-import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
-import { validateCPF } from "../utils/cpfValidator.js";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
+import { validateCPF } from '../utils/cpfValidator.js';
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "O nome é obrigatório."],
+    required: [true, 'O nome é obrigatório.'],
   },
   email: {
     type: String,
-    required: [true, "O email é obrigatório."],
+    required: [true, 'O email é obrigatório.'],
     unique: true,
     lowercase: true,
     match: [/\S+@\S+\.\S+/, 'Por favor, use um email válido.'],
   },
   cpf: {
     type: String,
-    required: [true, "O CPF é obrigatório."],
+    required: [true, 'O CPF é obrigatório.'],
     unique: true,
     validate: {
       validator: validateCPF,
-      message: props => `${props.value} não é um CPF válido!`
-    }
+      message: (props) => `${props.value} não é um CPF válido!`,
+    },
   },
   password: {
     type: String,
-    required: [true, "A senha é obrigatória."],
+    required: [true, 'A senha é obrigatória.'],
     select: false,
   },
+  passwordResetToken: String,
+  passwordResetExpires: Date,
 }, {
   timestamps: true,
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
@@ -43,6 +46,19 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
+
+const User = mongoose.model('User', userSchema);
 
 export default User;
