5e88a4973791a5d8d8218c01cdb49704a8028d21
Edu1059 <eduardoaffkaspro@gmail.com>
2025-11-18 00:31:27 -0300

    redefinindo os calculos e adicionando a funcao de excluir o historico

--- src/controllers/calc.controller.js
+++ src/controllers/calc.controller.js
@@ -1,6 +1,6 @@
 import HistoricoModel from "../models/historico.models.js";
 
-export const getCalc = async (req, res) => {
+export const getCalculo = async (req, res) => {
     try {
         const userId = req.user.id;
         const historicoUsuario = await HistoricoModel.find({usuarioId: userId})
@@ -11,25 +11,50 @@ export const getCalc = async (req, res) => {
     }
 }
 
-export const calc = async (req, res) => {
+export const calcular = async (req, res) => {
     try {
-        const { tipo, valorInicial, dataInicial, dataFinal, rentabilidade} = req.body;
+        const {tipo, valorInicial, dataInicial, dataFinal, rentabilidade, aporte, salvarHistorico} = req.body;
         const userId = req.user.id;
-        
-        let impostoRenda, impostoIOF, diferencaDias = dataFinal - dataInicial;
 
-        // Fórmula de Juros compostos
-        const resultado = valorInicial * Math.pow((1 + (rentabilidade / 100)), (diferencaDias / 365));
+        let resultado = 0.0,
+            impostoRenda = 0.0, 
+            impostoIOF = 0.0, 
+            diferencaDias = dataFinal - dataInicial,
+            meses = (diferencaDias / 30).toFixed(0)
+
+            
+        // Fórmula de Juros compostos, verificando o aporte
+        if (aporte === 0) {
+
+            resultado = valorInicial * Math.pow(1 + rentabilidade / 100, diferencaDias / 365);
+
+        } else {
+
+            let saldo = valorInicial;
+            const taxaMensal = Math.pow(1 + rentabilidade / 100, 1 / 12); 
+
+            for (let i = 1; i <= meses; i++) {
+                saldo = saldo * taxaMensal;  
+                saldo += aporte;             
+            }
+
+            resultado = saldo;
+        }
 
         // Cálculo de IOF
         if(diferencaDias <= 30) {
+
             impostoIOF = (diferencaDias * 30) - ((diferencaDias / 3) - 1) * 100;
+
         }
 
         //Valor do Imposto de Renda
         if(tipo === "lci" || tipo === "lca") {
+
             impostoRenda = 0.0; 
+
         } else {
+
             if(diferencaDias <= 180) {
                 impostoRenda = 0.225;
             } else if(diferencaDias <= 360) {
@@ -39,6 +64,7 @@ export const calc = async (req, res) => {
             } else {
                 impostoRenda = 0.150;
             }  
+
         }
         
         const rendimentoBruto = resultado - valorInicial;       
@@ -47,20 +73,52 @@ export const calc = async (req, res) => {
         const impostosTotais = valorImpostoIOF + valorImpostoIR;
         const lucroLiquido = resultado - impostosTotais;          
 
-        const newHistorico = await HistoricoModel.create({
+        const resultdoSimulacao = {
             usuarioId: userId,
             tipo,
             valorInicial,
+            valorAporte: aporte,
             tempoDias: diferencaDias,
             rentabilidade,
-            rendimentoBruto: rendimentoBruto.toFixed(2),
-            impostoIOF: valorImpostoIOF.toFixed(2),
-            impostoRenda: valorImpostoIR.toFixed(2),
-            lucroLiquido: lucroLiquido.toFixed(2),
-        })
+            rendimentoBruto: Number(rendimentoBruto.toFixed(2)),
+            rendimentoAporte: aporte * meses,
+            impostoIOF: Number(valorImpostoIOF.toFixed(2)),
+            impostoRenda: Number(valorImpostoIR.toFixed(2)),
+            lucroLiquido: Number(lucroLiquido.toFixed(2)),
+        }
+
+        if(salvarHistorico === 'sim') {
+            const newHistorico = await HistoricoModel.create({
+                ...resultdoSimulacao
+            })
+            return res.status(200).json({msg: newHistorico})
+        }
         
-        return res.status(200).json({ msg: newHistorico });
+        return res.status(200).json({ msg: resultdoSimulacao });
     } catch (error) {
         return res.status(500).json({ msg: error.message })
+    }
+}
+
+export const deleteHistorico = async (req, res) => {
+    const {idHistorico, deletarTudo} = req.body;
+    const userId = req.user.id;
+
+    try {
+
+         if(deletarTudo === "sim") {
+            await HistoricoModel.deleteMany({usuarioId: userId})
+            return res.status(200).json({msg: `Historico do usuário ${userId} deletado com sucesso`})
+        }  
+
+        const deletar = await HistoricoModel.deleteOne({_id: idHistorico, usuarioId: userId});
+
+        if (deletar.deletedCount === 0) {
+            return res.status(403).json({ msg: "Nada encontrado ou não pertence ao usuário" });
+        }
+        
+        return res.status(200).json({msg: "Histórico excluído com sucesso!"});
+    } catch (error) {
+        return res.status(500).json({error: error.message});
     }
 }
